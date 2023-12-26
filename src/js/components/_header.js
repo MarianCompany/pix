439baf176c5c65223744094fba022331e280a1ae
Marinad <samojlenko.wot@gmail.com>
2023-12-26 18:30:39 +0300

    Добавил выпадашки в меню, добавил игры в навигацию мобилки

--- src/js/components/_header.js
+++ src/js/components/_header.js
@@ -10,4 +10,24 @@ $(document).ready(function () {
             $('.header__nav').addClass('header__nav_active');
         }
     });
+
+    $('.header__nav-el-with-dropdown').on('click', function(e) {
+        e.stopPropagation();
+        const dropdown = $(this).find('.header__nav-dropdown');
+
+        $('.header__nav-dropdown_active').removeClass('header__nav-dropdown_active');
+
+        if (!dropdown.hasClass('header__nav-dropdown_active')) {
+            dropdown.addClass('header__nav-dropdown_active');
+        }
+    });
+
+    $(window).on('click', function() {
+        $('.header__nav-dropdown_active').removeClass('header__nav-dropdown_active');
+    });
+
+    $('.close-menus').on('click', function(e) {
+        e.stopPropagation();
+        $('.header__nav-dropdown_active').removeClass('header__nav-dropdown_active');
+    })
 });
